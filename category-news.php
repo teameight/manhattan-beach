1469bcea1b795b5ec52dacd8f71fb2c998addca4
Andrew Mowe <andrew@team-eightcom>
2017-10-02 13:49:25 -0400

    adjustments to news page

--- category-news.php
+++ category-news.php
@@ -9,14 +9,62 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area two-column">
+	<div id="primary" class="content-area two-column two-column-wide">
+		<aside class="sidebar calendar-sidebar">
+			<h3 class="sidebar-title">Calendar</h3>
+			<?php
+				date_default_timezone_set('America/New_York');
+
+				$args = array(
+					'category_name' => 'engagements',
+					'meta_key'     => 'event_date',
+					'meta_value'   => date( "Ymd" ), // change to how "event date" is stored
+					'meta_compare' => '>'
+				);
+
+				$query = new WP_Query($args);
+
+				if ( $query->have_posts() ) {
+					while ( $query->have_posts() ) : $query->the_post();
+						$eDate = get_field('event_date');
+						$eTitle = get_field('event_title');
+						$eTime = get_field('event_time');
+						$eDesc = get_field('event_description');
+						$eLocation = get_field('event_location');
+						$link = get_field('event_page_link');
+
+						if ($eDate && $eTitle) : ?>
+
+							<div class="calendar-event">
+								<span class="calendar-event-date"><?php echo $eDate; ?></span>
+								<?php if ( $link ) echo '<a class="calendar-event-link" href="'.$link['url'].'" target="'.$link['target'].'">'; ?>
+								<span class="calendar-event-title"><?php echo $eTitle; ?></span>
+								<?php if ( $link ) echo '</a>'; ?>
+								<?php if ( $eLocation ) echo '<span class="calendar-event-location">'.$eLocation.'</span>'; ?>
+								<?php if ( $eTime ) echo '<span class="calendar-event-time">'.$eTime.'</span>'; ?>
+
+								<?php if ( $eDesc ) echo '<span class="calendar-event-desc">'.$eDesc.'</span>'; ?>
+							</div>
+
+						<?php
+						endif;
+
+					endwhile;
+					wp_reset_postdata();
+
+				} else {
+					echo 'no upcoming events';
+				}
+			?>
+		</aside>
+
 		<main id="main" class="site-main">
 
 		<?php
 		if ( have_posts() ) : ?>
 
 			<header class="page-header extra-margin-heading">
-				<h1 class="entry-title news-heading">News &amp; Engagements</h1>
+				<h1 class="entry-title news-heading">News &amp; Events</h1>
 				<?php the_archive_description( '<div class="archive-description">', '</div>' ); ?>
 			</header><!-- .page-header -->
 
@@ -42,43 +90,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 
-		<aside class="sidebar calendar-sidebar">
-			<h3 class="sidebar-title">Calendar</h3>
-			<?php
-				date_default_timezone_set('America/New_York');
 
-				$args = array(
-					'category_name' => 'engagements',
-					'meta_key'     => 'event_date',
-					'meta_value'   => date( "Ymd" ), // change to how "event date" is stored
-					'meta_compare' => '>'
-				);
-
-				$query = new WP_Query($args);
-
-				if ( $query->have_posts() ) {
-					while ( $query->have_posts() ) : $query->the_post();
-						$link = get_field('event_page_link');
-						$eDate = get_field('event_date');
-
-						if ($link || !$link ) : ?>
-
-							<div class="calendar-event">
-								<span class="calendar-event-date"><?php echo $eDate; ?></span>
-								<a class="calendar-event-link" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
-							</div>
-
-						<?php
-						endif;
-
-					endwhile;
-					wp_reset_postdata();
-
-				} else {
-					echo 'no upcoming events';
-				}
-			?>
-		</aside>
 	</div><!-- #primary -->
 
 <?php
