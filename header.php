c90425705ef77aeb047515a21b43b61c2a1e2c95
Andrew Mowe <andrew@team-eightcom>
2017-10-20 14:17:08 -0400

    add Google Analytics

--- header.php
+++ header.php
@@ -13,13 +13,16 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- Google Tag Manager -->
-	<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-PCRDVJP');</script> -->
-	<!-- End Google Tag Manager -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108385894-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108385894-1');
+</script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -49,12 +52,6 @@
 	  <span class="tcon-visuallyhidden">Close</span>
 	</button>
 </div>
-
-	<!-- Google Tag Manager (noscript) -->
-<!-- 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCRDVJP"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
-	<!-- End Google Tag Manager (noscript) -->
-
 
 <?php if ( $static_wave ) { ?>
 
